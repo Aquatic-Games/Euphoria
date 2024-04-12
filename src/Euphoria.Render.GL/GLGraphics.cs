68f4887bb27a4fa1999cdbf568b32b508dbeef4d
ollie <ollie@ollierobinson.co.uk>
2024-04-12 19:25:14 +0100

    started readding opengl

---
+++ src/Euphoria.Render.GL/GLGraphics.cs
@@ -0,0 +1,14 @@
+﻿namespace Euphoria.Render.GL;
+
+public class GLGraphics : Graphics
+{
+    public override void Present()
+    {
+        throw new NotImplementedException();
+    }
+
+    public override void Dispose()
+    {
+        throw new NotImplementedException();
+    }
+}
