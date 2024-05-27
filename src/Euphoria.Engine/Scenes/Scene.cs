a6b91d39a626e09b8e807442d75709e6011a5d29
ollie <ollie@ollierobinson.co.uk>
2024-05-28 00:40:25 +0100

    begin scenes

---
+++ src/Euphoria.Engine/Scenes/Scene.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace u4.Engine.Scenes;
+
+public abstract class Scene : IDisposable
+{
+    private Dictionary<string, >
+    
+    public virtual void Dispose() { }
+}
