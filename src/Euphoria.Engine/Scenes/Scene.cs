d834a370b86f4869745ffbd1ab59111bcd97fba8
ollie <ollie@ollierobinson.co.uk>
2024-05-28 01:21:15 +0100

    basic fastlist

--- src/Euphoria.Engine/Scenes/Scene.cs
+++ src/Euphoria.Engine/Scenes/Scene.cs
@@ -1,11 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
+using u4.Core;
+using u4.Engine.Entities;
 
 namespace u4.Engine.Scenes;
 
 public abstract class Scene : IDisposable
 {
-    private Dictionary<string, >
+    private FastList<Entity> _entities;
+    private Dictionary<string, uint> _entityPointers;
+
+    public Scene()
+    {
+        _entities = new FastList<Entity>();
+        _entityPointers = new Dictionary<string, uint>();
+    }
     
     public virtual void Dispose() { }
 }
--- tests/Tests.Engine/Program.cs
+++ tests/Tests.Engine/Program.cs
@@ -1,5 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System;
+using u4.Core;
 
-using System;
+FastList<int> numbers = new FastList<int>();
+numbers.Add(1);
+numbers.Add(2);
+numbers.Add(3);
+numbers.Add(4);
+numbers.Add(5);
 
-Console.WriteLine("Hello, World!");
+Console.WriteLine(numbers.Array.Length);
+Console.WriteLine(numbers.Length);
